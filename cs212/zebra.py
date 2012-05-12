463f2bcd9f651707c6bbfea561fdf33b0f3fb8f3
Dheeraj Sayala <dheerosaur@gmail.com>
2012-05-12 16:39:02 +0530

    Added timing utilities timedcall, timedcalls

--- cs212/zebra.py
+++ cs212/zebra.py
@@ -23,7 +23,9 @@
 # Each house is painted a different color, and their inhabitants are of different nationalities,
 # own different pets, drink different beverages and smoke different brands of American cigarettes.
 
+import time
 import itertools
+
 
 def imright(h1, h2):
     "House h1 is immediately right of h2 if h1-h2 == 1."
@@ -58,6 +60,29 @@ def zebra_puzzle():
                 if nextto(Chesterfields, fox)
                 if nextto(Kools, horse)
                 )
+
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+def average(numbers):
+    "Return the average (arithmetic mean) of a sequence of numbers."
+    return sum(numbers) / float(len(numbers)) 
+
+def timedcalls(n, fn, *args):
+    """If n is an int, call fn(*args) n times. If it is a float,
+    call it till n seconds are over. Return min, avg, max of times"""
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for i in range(n)]
+    else:
+        times = []
+        while sum(times) < n:
+            times.append(timedcall(fn, *args)[0])
+    return min(times), average(times), max(times)
 
 if __name__ == '__main__':
     print "%s drinks water and %s owns the zebra" % zebra_puzzle()
